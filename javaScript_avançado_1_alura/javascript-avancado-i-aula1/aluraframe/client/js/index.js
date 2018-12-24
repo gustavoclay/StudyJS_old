cd296228ec02ede079c7541f567d8744821e6318
Gustavo Clay <gustavo.lima@eigmercados.com.br>
2018-12-24 15:51:17 -0200

    Continuar aula 04

---
+++ javaScript_avançado_1_alura/javascript-avancado-i-aula1/aluraframe/client/js/index.js
@@ -0,0 +1,47 @@
+var campos = [
+	document.querySelector("#data"),
+	document.querySelector("#quantidade"),
+	document.querySelector("#valor"),
+];
+
+
+console.log(campos);
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector(".form").addEventListener("submit", function (event) {
+
+	event.preventDefault();
+
+	var tr = document.createElement('tr');
+
+	campos.forEach(function (campo) {
+
+		var td = document.createElement('td');
+		td.textContent = campo.value;
+		tr.appendChild(td);
+
+	});
+
+	var tdVolume = document.createElement('td');
+	tdVolume.textContent = campos[1].value * campos[2].value;
+
+
+	tr.appendChild(tdVolume);
+
+
+	tbody.appendChild(tr);
+
+
+	campos[0] = '';
+	console.log(	campos[0]);
+	campos[1] = 1;
+	campos[2] = 0;
+
+	campo[0].focus();
+
+
+
+});
+
+
