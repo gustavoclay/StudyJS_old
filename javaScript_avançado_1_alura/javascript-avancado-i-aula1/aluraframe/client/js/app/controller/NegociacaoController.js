46c7364c76311a69e7835e2ad568bcc80fcea4c0
Gustavo Clay <gustavo.lima@eigmercados.com.br>
2018-12-26 11:15:37 -0200

    Continuar do capitulo 05 Temos o modelo, mas e a view?

--- javaScript_avançado_1_alura/javascript-avancado-i-aula1/aluraframe/client/js/app/controller/NegociacaoController.js
+++ javaScript_avançado_1_alura/javascript-avancado-i-aula1/aluraframe/client/js/app/controller/NegociacaoController.js
@@ -7,21 +7,35 @@ class NegociacaoController {
 		this._inputData = $('#data');
 		this._inputQuantidade = $('#quantidade');
 		this._inputValor = $('#valor');
+		this._listaNegociacoes = new ListaNegociacoes();
+
 	}
 
 	adiciona(event) {
 		event.preventDefault();
+		this._listaNegociacoes.adiciona(this._criaNegociacao());
+		this._limpaFormulario();
 
-		let data = new Date(this._inputData.value);
-		console.log(data);
+		console.log(this._listaNegociacoes.negociacoes);
+
+	}
+
+	_criaNegociacao() {
+
 		let negociacao = new Negociacao(
-			data,
+			DateUtil.textoParaData(this._inputData.value),
 			this._inputQuantidade.value,
 			this._inputValor.value,
 		);
 
-		console.log(negociacao);
+	}
 
+	_limpaFormulario() {
+		this._inputData.value = '';
+		this._inputQuantidade.value = 1;
+		this._inputValor.value = 0;
+
+		this._inputData.focus();
 	}
 
 
