c1fcd25ad7e1f2f508ef4135dfb0d26102db228c
Gustavo Clay <gustavoclay@gmail.com>
2018-12-26 15:35:52 -0200

    Curso finalizado.

--- javaScript_avançado_1_alura/javascript-avancado-i-aula1/aluraframe/client/js/app/view/NegociacoesView.js
+++ javaScript_avançado_1_alura/javascript-avancado-i-aula1/aluraframe/client/js/app/view/NegociacoesView.js
@@ -1,12 +1,11 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
 	constructor(elemento) {
 
-		this._elemento = elemento;
+		super(elemento);
 	}
 
-
-	_template(model) {
+	template(model) {
 
 		return `
 			<table class="table table-hover table-bordered">
@@ -44,30 +43,5 @@ class NegociacoesView {
 					`;
 	}
 
-	update(model) {
-		this._elemento.innerHTML = this._template(model);
-
-	}
-
-
 }
 
-
-/*
-<table class="table table-hover table-bordered">
-		<thead>
-				<tr>
-					<th>DATA</th>
-							<th>QUANTIDADE</th>
-							<th>VALOR</th>
-							<th>VOLUME</th>
-				</tr>
-		</thead>
-
-		<tbody>
-		</tbody>
-
-		<tfoot>
-		</tfoot>
-</table>
-*/
