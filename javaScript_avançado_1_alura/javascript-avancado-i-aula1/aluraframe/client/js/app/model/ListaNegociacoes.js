42a8904de1544210e765eef5704005348b27d17c
Gustavo Clay <gustavoclay@gmail.com>
2018-12-26 17:07:51 -0200

    Continuar 06 Método que não altera propriedade

--- javaScript_avançado_1_alura/javascript-avancado-i-aula1/aluraframe/client/js/app/model/ListaNegociacoes.js
+++ javaScript_avançado_1_alura/javascript-avancado-i-aula1/aluraframe/client/js/app/model/ListaNegociacoes.js
@@ -1,9 +1,7 @@
 class ListaNegociacoes {
-	constructor(contexto, armadilha) {
+	constructor() {
 
 		this._negociacoes = [];
-		this._armadilha = armadilha;
-		this._contexto = contexto;
 	}
 
 	get negociacoes() {
@@ -14,15 +12,11 @@ class ListaNegociacoes {
 	adiciona(negociacao) {
 
 		this._negociacoes.push(negociacao);
-		//this._armadilha(this);
-		Reflect.apply(this._armadilha, this._contexto, [this]);
 	}
 
 	esvazia() {
 
 		this._negociacoes = [];
-		//this._armadilha(this);
-		Reflect.apply(this._armadilha, this._contexto, [this]);
 	}
 
 
