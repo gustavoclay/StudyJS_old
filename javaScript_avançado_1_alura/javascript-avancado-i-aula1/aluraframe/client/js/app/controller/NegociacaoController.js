3bec4a699ab1132b52eb304f134bc800d26fb2f3
Gustavo Clay <gustavoclay@gmail.com>
2018-12-26 16:15:00 -0200

    Continuar 04 Arrow function e seu escopo léxico

--- javaScript_avançado_1_alura/javascript-avancado-i-aula1/aluraframe/client/js/app/controller/NegociacaoController.js
+++ javaScript_avançado_1_alura/javascript-avancado-i-aula1/aluraframe/client/js/app/controller/NegociacaoController.js
@@ -7,10 +7,13 @@ class NegociacaoController {
 		this._inputData = $('#data');
 		this._inputQuantidade = $('#quantidade');
 		this._inputValor = $('#valor');
-		this._listaNegociacoes = new ListaNegociacoes();
+
+		this._listaNegociacoes = new ListaNegociacoes(this, function (model) {
+
+			this._negociacoesView.update(model);
+		});
 
 		this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-		this._negociacoesView.update(this._listaNegociacoes);
 
 		this._mensagem = new Mensagem();
 		this._mensagemView = new MensagemView($('#mensagemView'));
@@ -24,12 +27,18 @@ class NegociacaoController {
 		this._negociacoesView.update(this._listaNegociacoes);
 
 		this._mensagem.texto = 'Negociação adicionada  com sucesso';
-		this._mensagemView.update(this._mensagem);
 
 		this._limpaFormulario();
 
 	}
 
+	apaga() {
+
+		this._listaNegociacoes.esvazia();
+
+		this._mensagem.texto = 'Negociações apagadas com sucesso';
+		this._mensagemView.update(this._mensagem);
+	}
 
 	_criaNegociacao() {
 
--- javaScript_avançado_1_alura/javascript-avancado-i-aula1/aluraframe/client/js/app/model/ListaNegociacoes.js
+++ javaScript_avançado_1_alura/javascript-avancado-i-aula1/aluraframe/client/js/app/model/ListaNegociacoes.js
@@ -1,14 +1,28 @@
 class ListaNegociacoes {
-	constructor() {
-		this._negociacoes = [];
-	}
+	constructor(contexto, armadilha) {
 
-	adiciona(negociacao) {
-		this._negociacoes.push(negociacao);
+		this._negociacoes = [];
+		this._armadilha = armadilha;
+		this._contexto = contexto;
 	}
 
 	get negociacoes() {
+
 		return [].concat(this._negociacoes);
+	}
+
+	adiciona(negociacao) {
+
+		this._negociacoes.push(negociacao);
+		//this._armadilha(this);
+		Reflect.apply(this._armadilha, this._contexto, [this]);
+	}
+
+	esvazia() {
+
+		this._negociacoes = [];
+		//this._armadilha(this);
+		Reflect.apply(this._armadilha, this._contexto, [this]);
 	}
 
 
